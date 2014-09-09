eb65c56eb04274d8f1e54d9e38afe79aba44638a
Maximilian Berger <snowdragon92@gmail.com>
2014-09-09 16:01:27 +0200

    Added setup.py to build an .exe using cx_freeze
    Installed cx_freeze with a patch from:
    http://www.lfd.uci.edu/~gohlke/pythonlibs/#cx_freeze
    Regarding this issue:
    
    https://groups.google.com/forum/#!msg/comp.lang.python/Ubagx7NDtJE/_47ZP2Uj-BUJ

---
+++ setup.py
@@ -0,0 +1,38 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+copyfiles = [
+    ('C:\Python34\Lib\site-packages\PyQt5\libEGL.dll', 'libEGL.dll')]
+buildOptions = dict(packages=[], excludes=[], include_files=copyfiles)
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [
+    Executable(
+        'TardisDiff.py',
+        base=base,
+        shortcutName="TardisDiff",
+        shortcutDir="DesktopFolder",
+        #icon="tardis.ico" Google for a fancy tardis icon until I've made one myself.
+        )
+]
+
+options = {
+    'build_exe': {
+        'include_files': [],
+        'path': sys.path + ['modules']
+    }
+}
+
+setup(name='TardisDiff',
+      version='0.1',
+      description='TardisDiff is a tool to output the time you worked today.',
+      options=dict(build_exe=buildOptions),
+      executables=executables
+      )
